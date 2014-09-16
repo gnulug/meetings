e3eb96a80832f8bdca45eb9d14a3bcfdcbe1c134
Harsh Singh <hisingh1@gmail.com>
2014-09-15 23:00:25 -0500

    Update hello.sh

--- meeting2-09152014/hello.sh
+++ meeting2-09152014/hello.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
+
+use quotes to preserve newlines. EX: 
+    x=$(ifconfig)
+    echo $x # no new lines
+    echo "$x" # new lines 
+# having new lines is useful for grep
+
+# spaces matter in bash scripts b.c if bash sees x = 5 it will run the
+# program x and pass in the arguments = and 5
+
 x="hello"
 
-if [ "$x" == "hello" ]; then
+if [ "$x" == "hello" ]; then # Note the 4 spaces between []
     echo "If statement pass!"
 else
     echo "if statement fail."
